c177dc6578f6a09dd6f1f3cc8f3be2e57609e80b
Willian Carlos Gomes <gomes.cwillian@gmail.com>
2023-09-12 22:04:32 -0300

    Arquitetura inicial do FrontEnd. Header, Aside e Main.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
+import { Github, FileVideo } from 'lucide-react'
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
 
 export function App() {
   return (
-    <h1>Hello World</h1>
-    )
+    <div className='min-h-screen flex flex-col'>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvolvido por Willian Carlos Gomes no NLW da Rocketseat</span>
+
+          <Separator orientation='vertical' className='h-6' />
+
+          <Button variant={"outline"}>
+            <Github className='w-4 h4 mr-2'></Github>
+            GitHub
+          </Button>
+        </div>
+      </div>
+
+      <main className='flex-1 p-6 flex gap-6'>
+        <div className='flex flex-col flex-1 gap-4'>
+          <div className='grid grid-rows-2 gap-4 flex-1'>
+            <Textarea className='resize-none p-4 leading-relaxed' placeholder='Inclua o prompt para a IA'></Textarea>
+            <Textarea className='resize-none p-4 leading-relaxed' placeholder='Resultado gerado pela IA...' readOnly></Textarea>
+          </div>
+          <p className='text-sm text-muted-foreground'>Lembre-se: Você pode utilizar a variável <code className='text-violet-400'>{'{transcription}'}</code>  no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
+
+        </div>
+
+
+        <aside className='w-80 -space-y-6'>
+          <form className='space-y-6'>
+            <label htmlFor="video" className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'>
+              <FileVideo className='w-8 h-8'></FileVideo>
+              Selecione um vídeo
+              </label>
+
+            <input type="file" id="video" accept='video/mp4' className='sr-only' />
+
+          <Separator />
+
+          <div className='space-y-1'></div>
+
+          </form>
+
+        </aside>
+
+      </main>
+
+    </div>
+
+  )
 }
 
